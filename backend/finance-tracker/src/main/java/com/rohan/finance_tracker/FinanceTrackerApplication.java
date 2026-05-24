e5dc89d4e59fb2e337d4990b88d65ce03e7b32e8
Rohan <rohandhodare14@gmail.com>
2026-05-25 00:56:32 +0530

    feat: Inital project setup

---
+++ backend/finance-tracker/src/main/java/com/rohan/finance_tracker/FinanceTrackerApplication.java
@@ -0,0 +1,16 @@
+package com.rohan.finance_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
+
+@SpringBootApplication
+public class FinanceTrackerApplication {
+
+	public static void main(String[] args) {
+		System.out.println("JVM TZ = " + TimeZone.getDefault().getID());
+		SpringApplication.run(FinanceTrackerApplication.class, args);
+	}
+
+}
